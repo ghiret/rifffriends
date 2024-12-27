cced1f68d5bbe63f839f08d3ff4bc45f75a0aec2
Paco Cordobes <ghiret@gmail.com>
2024-12-27 23:12:24 +0100

    Initialize Django project structure with basic settings, models, views, and tests

---
+++ home/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+from django.http import HttpResponse, HttpRequest
+
+def credits(request: HttpRequest):
+    content = "<html><body><p>Example website built following Django In Action chapters by Paco.</p></body></html>"
+    return HttpResponse(content=content, content_type="text/html")
