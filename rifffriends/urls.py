23a0b4f407e7460cd2723b48f149996380d13d84
Paco Cordobes <ghiret@gmail.com>
2024-12-27 23:19:36 +0100

    Add basic CI workflow and pre-commit hooks; update views and URL patterns

--- rifffriends/urls.py
+++ rifffriends/urls.py
@@ -19,7 +19,4 @@ from django.contrib import admin
 from django.urls import path
 from home import views as home_views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('credits/',home_views.credits)
-]
+urlpatterns = [path("admin/", admin.site.urls), path("credits/", home_views.credits)]
