959ea907321773f92bc47fce610a412abc28ceee
Paco Cordobes <ghiret@gmail.com>
2024-12-27 23:26:08 +0100

    End of chapter exercises: Add about and version views; update URL patterns

--- home/views.py
+++ home/views.py
@@ -1,7 +1,17 @@
 # Create your views here.
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
 
 
 def credits(request: HttpRequest):
+    content = "<html><body><p>Nicky/Paco.</p></body></html>"
+    return HttpResponse(content=content, content_type="text/html")
+
+
+def about(request: HttpRequest):
     content = "<html><body><p>Example website built following Django In Action chapters by Paco.</p></body></html>"
     return HttpResponse(content=content, content_type="text/html")
+
+
+def version(request: HttpRequest):
+    content = {"version": "0.0.1"}
+    return JsonResponse(content)
